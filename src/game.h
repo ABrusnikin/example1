5c76a0af023ac5636773c9f9744c4d6045f40258
Aldaanub <aldaanub@student.21-school.ru>
2025-02-05 17:58:35 +0700

    refactoring game header

--- src/game.h
+++ src/game.h
@@ -32,3 +32,29 @@ struct status
     int me_y;
     bool me_on_point;
 };
+
+int start_screen();
+void load_level_screen();
+
+void game_loop(struct status* Game);
+
+int download_map(struct status* Game);
+void clear_map(int n,int m, char** arr);
+void free_map(int n,char** arr);
+void fill_tile(int i, int j, struct status* Game);
+
+void display_map(struct status* Game);
+void move_me(int dx, int dy, struct status* Game);
+bool can_move_free(int dx, int dy, struct status* Game);
+void move_free(int dx, int dy, struct status* Game);
+bool can_push_box(int dx, int dy, struct status* Game);
+void push_box(int dx, int dy, struct status* Game);
+void reload_map(struct status* Game);
+
+void set_square(int dx, int dy, char c, struct status* Game);
+char next_square(int dx, int dy, struct status* Game);
+char next_2_square(int dx, int dy, struct status* Game);
+void move_my_coords(int dx, int dy, struct status* Game);
+void make_prev_square_empty(struct status* Game);
+
+void check_points(int dx, int dy, struct status* Game);
